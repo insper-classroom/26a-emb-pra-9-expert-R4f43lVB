ca74dcf7b219f06e86b1c60675bea7ff2b91990f
Rafael Bravo <rafaelvb@al.insper.edu.br>
2026-05-18 20:55:06 -0300

    Funcoes de uso do joystick

--- main/main.c
+++ main/main.c
@@ -7,14 +7,24 @@
 #include "pico/stdlib.h"
 #include "hardware/irq.h"
 #include "hardware/uart.h"
+#include "hardware/adc.h"
 #include "hc06.h"
 
 // PINS
+#define PIN_X 27
+#define PIN_Y 26
 #define BTN_PIN 16
-#define HC06_NAME "RAFA"
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
 
 // Semaforos
 SemaphoreHandle_t xSemaphorePIN;
+
+typedef struct adc {
+    int axis;
+    int value;
+} adc_t;
 
 typedef struct config_data {
     char *name;
@@ -26,23 +36,11 @@ QueueHandle_t xQueuePIN;
 QueueHandle_t xQueueConfig;
 QueueHandle_t xQueueRX;
 QueueHandle_t xQueueTX;
+QueueHandle_t xQueueADC;
 
 void uart_rx_handler() {
     uint8_t ch = uart_getc(HC06_UART_ID);
     xQueueSendFromISR(xQueueRX, &ch, 0);
-}
-
-void init_uart_hc06(void) {
-    uart_init(HC06_UART_ID, HC06_BAUD_RATE);
-
-    gpio_set_function(HC06_TX_PIN, UART_FUNCSEL_NUM(HC06_UART_ID, HC06_TX_PIN));
-    gpio_set_function(HC06_RX_PIN, UART_FUNCSEL_NUM(HC06_UART_ID, HC06_RX_PIN));
-
-    int __unused actual = uart_set_baudrate(HC06_UART_ID, HC06_BAUD_RATE);
-
-    uart_set_hw_flow(HC06_UART_ID, false, false);
-
-    uart_set_format(HC06_UART_ID, 8, 1, UART_PARITY_NONE);
 }
 
 void init_uart_irq() {
@@ -88,26 +86,78 @@ static void serial_task(void* p) {
     }
 }
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (gpio == BTN_PIN && events == GPIO_IRQ_EDGE_FALL) {
-        xSemaphoreGiveFromISR(xSemaphorePIN, 0);
-    } 
+void x_task(void *p) {
+    adc_gpio_init(PIN_X);
+
+    adc_t data;
+    data.axis = 0;
+
+    int soma = 0;
+    int numeros[5] = {0, 0, 0, 0, 0};
+    while (true) {
+        int result;
+        adc_select_input(1);
+        result = ((int) adc_read() - 2047) / 8;
+
+        int velho = numeros[0];
+        for (int i = 0; i < 4; i++) numeros[i] = numeros[i+1];
+        numeros[4] = result;
+        soma += result - velho;
+        int media = soma / 5;
+        if (media > 50 || media < -50) {
+            data.value = media;
+            xQueueSend(xQueueADC, &data, pdMS_TO_TICKS(50));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
 
+void y_task(void *p) {
+    adc_gpio_init(PIN_Y);
 
-// Funcoes auxiliares
-void init_buttons(void) {
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
-    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    adc_t data;
+    data.axis = 1;
+
+    int soma = 0;
+    int numeros[5] = {0, 0, 0, 0, 0};
+    while (true) {
+        int result;
+        adc_select_input(0);
+        result = ((int) adc_read() - 2047) / 8;
+
+        int velho = numeros[0];
+        for (int i = 0; i < 4; i++) numeros[i] = numeros[i+1];
+        numeros[4] = result;
+        soma += result - velho;
+        int media = soma / 5;
+        if (media > 50 || media < -50) {
+            data.value = media;
+            xQueueSend(xQueueADC, &data, pdMS_TO_TICKS(50));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
 
+void uart_task(void *p) {
+    adc_t data;
 
-// Tasks
+    while (true) {
+        if (xQueueReceive(xQueueADC, &data, pdMS_TO_TICKS(50))) {
+            uart_putc(UART_ID, data.axis);
+            uart_putc(UART_ID, data.value);
+            uart_putc(UART_ID, (data.value >> 8));
+            uart_putc(UART_ID, -1);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+}
+
 void pin_task(void *p) {
     config_data_t data;
-    data.name = HC06_NAME;
+    data.name = "RAFA";
 
     char pin[5];
     srand(time_us_32());
@@ -123,6 +173,36 @@ void pin_task(void *p) {
     }
 }
 
+void btn_callback(uint gpio, uint32_t events) {
+    if (gpio == BTN_PIN && events == GPIO_IRQ_EDGE_FALL) {
+        xSemaphoreGiveFromISR(xSemaphorePIN, 0);
+    } 
+}
+
+
+// Funcoes auxiliares
+void init_buttons(void) {
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+}
+
+void init_uart_hc06(void) {
+    uart_init(HC06_UART_ID, HC06_BAUD_RATE);
+
+    gpio_set_function(HC06_TX_PIN, UART_FUNCSEL_NUM(HC06_UART_ID, HC06_TX_PIN));
+    gpio_set_function(HC06_RX_PIN, UART_FUNCSEL_NUM(HC06_UART_ID, HC06_RX_PIN));
+
+    int __unused actual = uart_set_baudrate(HC06_UART_ID, HC06_BAUD_RATE);
+
+    uart_set_hw_flow(HC06_UART_ID, false, false);
+
+    uart_set_format(HC06_UART_ID, 8, 1, UART_PARITY_NONE);
+}
+
+
+
 int main(void) {
     stdio_init_all();
     init_buttons();
@@ -132,8 +212,13 @@ int main(void) {
 
     xQueuePIN = xQueueCreate(32, sizeof(char[5]));
     xQueueConfig = xQueueCreate(32, sizeof(config_data_t));
+    xQueueRX = xQueueCreate(32, sizeof(uint8_t));
+    xQueueTX = xQueueCreate(32, sizeof(uint8_t));
+    xQueueADC = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(pin_task, "Task do PIN", 256, NULL, 1, NULL);
+    xTaskCreate(tx_task, "TX", 512, NULL, 2, NULL);
+    xTaskCreate(serial_task, "Serial", 1024, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
